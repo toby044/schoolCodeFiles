1d66099bd5c7f90c0d884f1b3447caa17705000b
toby044 <62332472+toby044@users.noreply.github.com>
2020-11-09 10:10:38 +0100

    Assignment1.1,1.2,1.3 finished

--- Assignment1.2/js/script.js
+++ Assignment1.2/js/script.js
@@ -1,18 +1,26 @@
 "use strict";
-let array = [];
 
-function throwDice () {
-    return Math.floor(Math.random()* 6) + 1;
+const $ = function (foo) {
+    return document.getElementById(foo);
 }
 
-let result = throwDice();
+let dice1 = $("myDice");
+let dice2 = $("myDice1");
+let dice3 = $("myDice2");
+let dice4 = $("myDice3");
+let dice5 = $("myDice4");
 
-document.getElementById("myDice").innerHTML = result;
 
-window.addEventListener("load" , throwDice);
+let array = [];
+for(let i = 0; i < 5; i++) {
+    function roll (foo) {
+        return Math.floor(Math.random() * foo) + 1;
+    }
+    array[i]=roll(6);
+}
 
-// array.push(result);
-
-// for(i = array[0]; array.length > 5}; i++) {
-//     return Math.floor(Math.random()*6) +1;
-// }
+dice1.innerHTML = array[0];
+dice2.innerHTML = array[1];
+dice3.innerHTML = array[2];
+dice4.innerHTML = array[3];
+dice5.innerHTML = array[4];
